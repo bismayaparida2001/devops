8a3fa633b0a3c3d2cdf1d2eff02cad632ff1bab3
bismayaparida2001 <100926460+bismayaparida2001@users.noreply.github.com>
2022-03-04 23:02:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = flask.Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home():
-    return"<h1>my first project</h1>"
+    return"<h1>my first project is working</h1>"
 if __name__=='__main__':
     app.run(debug=True,host="0.0.0.0")
